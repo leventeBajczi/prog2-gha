db9d81915be1836a4c4ab088d90e0d75ef513e4a
Levente Bajczi <bajczilevi@gmail.com>
2018-05-05 14:39:13 +0200

    initial VM commit

---
+++ src/virtualmachine/virtualmachine.hpp
@@ -0,0 +1,15 @@
+#ifndef _VIRTUALMACHINE
+#define _VIRTUALMACHINE
+
+#include <stack>
+#include <cstdint>
+
+class VirtualMachine
+{
+    private:
+        std::stack<uint8_t> stack;
+        
+
+};
+
+#endif
