239674a1643301279f333f99f414ab11a7f82f75
Levente Bajczi <bajczilevi@gmail.com>
2018-05-15 03:02:23 +0200

    added some fixis

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,20 @@
 
 int main(int argc, char** argv)
 {
-    std::string sprachendatei;
+
+
+
+std::string sprachendatei = "{  \
+                                                \"Move B to A\":\"mov\",\
+                                                \"Add B to A\":\"add\",\
+                                                \"Substract B from A\":\"sub\",\
+                                                \"Swap the upper and lower 4 bits\":\"swp\",\
+                                                \"Shift left, insert 0\":\"sl0\",\
+                                                \"Shift right, insert 0\":\"sr0\",\
+                                                \"Jump to subroutine\":\"jsr\"\
+                                                }";
+Sprache sprache(sprachendatei, "mylang");
+VirtualMachine* vm = new VirtualMachine(sprache);
     for(int i = 1; i<argc;i++){
         if      (strcmp(argv[i], PRINT_LANGUAGE) == 0){
             std::cout<<Sprache::printEmpty();
@@ -22,6 +35,11 @@ int main(int argc, char** argv)
             std::string file((std::istreambuf_iterator<char>(t)),
             std::istreambuf_iterator<char>());
             sprachendatei = file;
+
+            Sprache sprache(sprachendatei, "mylang");
+            delete vm;
+            vm = new VirtualMachine(sprache);
+
         }
         else if (strcmp(argv[i], USE_FILES) == 0){
             for(i=i+1;i<argc;i++)
@@ -29,7 +47,8 @@ int main(int argc, char** argv)
                 std::ifstream t(argv[i]);
                 std::string file((std::istreambuf_iterator<char>(t)),
                 std::istreambuf_iterator<char>());
-                
+                std::cout<<file<<std::endl;
+                vm->addSubroutine(file);
             }
         }
         else{
@@ -38,29 +57,10 @@ int main(int argc, char** argv)
         }
     }
 
+vm ->reRunAll();
+std::cout<<(unsigned int)vm->getValue("(r1)")<<std::endl;
 
-
-
-    std::string sprachendatei = "{  \
-   \"Move B to A\":\"mov\",\
-   \"Add B to A\":\"add\",\
-   \"Substract B from A\":\"sub\",\
-   \"Swap the upper and lower 4 bits\":\"swp\",\
-   \"Shift left, insert 0\":\"sl0\",\
-   \"Shift right, insert 0\":\"sr0\",\
-   \"Jump to subroutine\":\"jsr\"\
-}";
-
-std::string subroutine = "main\nmov r12 0x34\nadd r12 0x02\nmov r10 0x00\nmov (r10) r12";
-
-    Sprache sprache(sprachendatei, "mylang");
-    VirtualMachine vm(sprache);
-    vm.addSubroutine(subroutine);
-    vm.runInstruction("sub r12 0x05");
-    vm.runInstruction("jsr main");
-    std::cout<<(unsigned int)vm.getValue("(r10)")<<std::endl;
-    
-
+delete vm;
 
     return 0;
 }
